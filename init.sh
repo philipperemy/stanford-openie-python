b656a2c86b45d007fedd5dd2685ee87008bba053
philipperemy <premy@reactive.co.jp>
2016-12-23 13:52:58 +0100

    delete init

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir stanford-openie
-wget -nc http://nlp.stanford.edu/projects/naturalli/stanford-openie.jar
-wget -nc http://nlp.stanford.edu/projects/naturalli/stanford-openie-models.jar
-mv stanford-openie.jar stanford-openie-models.jar stanford-openie
-echo "Google bought IBM for 10 dollars." > samples.txt
-python main.py -f samples.txt -v
