29441d31c507839f79c717a00f79146bc7aaa11f
Philippe Remy <premy.enseirb@gmail.com>
2021-03-12 15:32:43 +0900

    1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='stanford-openie',
-    version='1.0.2',
+    version='1.0.3',
     description='Minimalist wrapper around Stanford OpenIE',
     author='Philippe Remy',
     license='MIT',
