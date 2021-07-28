d6b1fef33fa07e21c3030e2903004d1f4ad8abf5
Philippe Remy <premy.enseirb@gmail.com>
2021-07-28 08:52:58 +0200

    add properties

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from openie import StanfordOpenIE
 
-with StanfordOpenIE() as client:
+# https://stanfordnlp.github.io/CoreNLP/openie.html#api
+# Default value of openie.affinity_probability_cap was 1/3.
+properties = {
+    'openie.affinity_probability_cap': 2 / 3,
+}
+
+with StanfordOpenIE(properties=properties) as client:
     text = 'Barack Obama was born in Hawaii. Richard Manning wrote this sentence.'
     print('Text: %s.' % text)
     for triple in client.annotate(text):
